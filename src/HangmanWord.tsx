3e6fa44e73dcffe4e73d4eee99063ffa227f5b63
liuleora@gmail.com <liuleora@gmail.com>
2024-09-08 16:24:18 +0900

    Handles the word guessing

---
+++ src/HangmanWord.tsx
@@ -0,0 +1,42 @@
+type HangmmanWordProps = {
+    guessedLetters: string[];
+    wordToGuess: string;
+    reveal?: boolean;
+};
+export function HangmanWord({
+    guessedLetters,
+    wordToGuess,
+    reveal = false,
+}: HangmmanWordProps) {
+    return (
+        <div
+            style={{
+                display: "flex",
+                gap: ".25em",
+                fontSize: "6rem",
+                fontWeight: "bold",
+                textTransform: "uppercase",
+                fontFamily: "monospace",
+            }}
+        >
+            {wordToGuess.split("").map((letter, index) => (
+                <span style={{ borderBottom: ".1em solid black" }} key={index}>
+                    <span
+                        style={{
+                            visibility:
+                                guessedLetters.includes(letter) || reveal
+                                    ? "visible"
+                                    : "hidden",
+                            color:
+                                !guessedLetters.includes(letter) && reveal
+                                    ? "red"
+                                    : "black",
+                        }}
+                    >
+                        {letter}
+                    </span>
+                </span>
+            ))}
+        </div>
+    );
+}
